6a80eac5eced565dc1476edd23602fe88a0ebaaa
AnanyaKakaveti <a.kakaveti@ufl.edu>
2025-02-28 13:39:56 -0500

    ugly landing page created

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,15 +1,40 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet, Image } from "react-native";
+import TempLogo from '@/assets/images/Ekko_Temp_Logo.png'; // 1600 x 900
 
-export default function Index() {
+const HomeScreen = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Image source= { TempLogo } style={styles.image}/>
+      <Text style={styles.title}>Welcome to Ekko!</Text>
+      <Text style={styles.subtitle}>Your favorite music sharing app!</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
+    backgroundColor: '#2f2f2f',
+  },
+  image: {
+    width: 370,
+    height: 210,
+    marginBottom: 20,
+    borderRadius: 10,
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    color: '#fff',
+  },
+  subtitle: {
+    fontSize: 16,
+    color: '#fff',
+  },
+});
+
+export default HomeScreen;
