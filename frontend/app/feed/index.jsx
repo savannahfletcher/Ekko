6412f5d01dc2ff986c831814396e863719fbbed2
Savannah Fletcher <sfletcher777@gmail.com>
2025-03-06 13:46:42 -0500

    Profile pics added to posts in feed

--- frontend/app/feed/index.jsx
+++ frontend/app/feed/index.jsx
@@ -4,15 +4,16 @@ import { View, Text, StyleSheet, FlatList, Image } from 'react-native'
 import espresso from '@/assets/images/songImages/Espresso.jpg';
 import juna from '@/assets/images/songImages/Juna.jpeg';
 import lalalala from '@/assets/images/songImages/LALALALA.jpeg';
-
-
+import profilePic1 from '@/assets/images/profileImages/profilePic1.jpg';
+import profilePic2 from '@/assets/images/profileImages/profilePic2.jpg';
+import profilePic3 from '@/assets/images/profileImages/profilePic3.jpg';
 
 const FeedScreen = () => {
 
     const [posts, setPosts] = useState([
-        {id: '1', username: 'Carl Smith', songName: 'Espresso', albumCover: espresso,artist: 'Sabrina Carpenter'},
-        {id: '2', username: 'Gloria Shell', songName: 'Juna', albumCover: juna, artist: 'Clairo'},
-        {id: '3', username: 'Aria Coolidge', songName: 'LALALALA', albumCover: lalalala, artist: 'Stray Kids'},
+        {id: '1', profile: profilePic1, username: 'Carl Smith', songName: 'Espresso', albumCover: espresso, artist: 'Sabrina Carpenter'},
+        {id: '2', profile: profilePic2, username: 'Gloria Shell', songName: 'Juna', albumCover: juna, artist: 'Clairo'},
+        {id: '3', profile: profilePic3, username: 'Aria Coolidge', songName: 'LALALALA', albumCover: lalalala, artist: 'Stray Kids'},
     ])
 
     return (
@@ -28,10 +29,14 @@ const FeedScreen = () => {
                         end={{ x: 0.5, y: 0.7 }}
                         
                         style={styles.postItem}>
-                            
-                        <Text style = {styles.postUsername}>
-                            {item.username}
-                        </Text>
+                        <View style={styles.postHeader}> 
+                            <Image source={item.profile} style={styles.profilePic}/>
+
+                            <Text style = {styles.postUsername}>
+                                {item.username}
+                            </Text>
+                        </View>
+
                         <Image source= { item.albumCover } style={styles.image}/>
                         <Text style = {styles.postSongName}>
                             {item.songName}
@@ -58,11 +63,16 @@ const styles = StyleSheet.create ({
         marginBottom: 50,
         borderRadius: 19,
     },
+    postHeader: { // contains the profile picture and username
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginBottom: 10,
+    },
     postUsername: {
         fontSize: 16,
         fontWeight: 'bold',
         color: '#fff',
-        paddingLeft: 2,
+        paddingLeft: 5,
         paddingBottom: 5,
     },
     postSongName: {
@@ -76,7 +86,11 @@ const styles = StyleSheet.create ({
         color: '#A7A7A7',
         paddingLeft: 2,
     },
-
+    profilePic: {
+        width: 50,
+        height: 50,
+        borderRadius: 50,
+    },
     image: {
         width: 300,
         height: 300,
